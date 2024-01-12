909f9e2246ecdd5c2e08e5b5fb871548c628a275
Abdullah Al Arif <140299847+arif-xs@users.noreply.github.com>
2024-01-12 23:30:18 +0600

    game and winner conditions added

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -17,11 +17,21 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
+function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = "Next Player " + (xIsNext ? "X" : "0");
+  }
+
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -35,6 +45,7 @@ export default function Board() {
   }
   return (
     <>
+      <div>{status}</div>
       <div className="flex">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -52,4 +63,35 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+export default function Game() {
+  return (
+    <div>
+      <div>
+        <Board />
+      </div>
+      <div>{/*TBD*/}</div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
